a43eb6ff7f72b07407657bdf00bec92c97d64385
Andras Gyömrey <andras@gyoemrey.me>
2025-12-20 14:29:47 +1000

    remove client info

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,13 +1,3 @@
-output "client_id" {
-  description = "The client ID for the OAuth 2.0 client."
-  value       = google_iap_client.obsidian_gcal_sync.client_id
-}
-
-output "client_secret" {
-  description = "The client ID for the OAuth 2.0 client."
-  value       = google_iap_client.obsidian_gcal_sync.secret
-  sensitive   = true
-}
 
 output "project_id" {
     description = "The ID of the created project"
